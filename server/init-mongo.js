e9ae07d4a6632fcc727645f4da33e0f7227bd39c
Paul Zhu <zhz433@ucsd.edu>
2020-04-11 18:09:21 -0700

    docker setup

---
+++ server/init-mongo.js
@@ -0,0 +1,13 @@
+// Not used
+db.createUser(
+    {
+//        user : "memegleapp",
+//        pwd : "memegleapp",
+        roles :[
+            {
+                role : "readWrite",
+                db : "local"
+            }
+        ]
+    }
+)
--- server/src/main/java/com/memegle/server/controller/PictureController.java
+++ server/src/main/java/com/memegle/server/controller/PictureController.java
@@ -2,6 +2,8 @@ package com.memegle.server.controller;
 
 import com.memegle.server.model.Picture;
 import com.memegle.server.model.PictureRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,8 +15,8 @@ import java.util.List;
 
 @RestController
 public class PictureController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PictureController.class);
     // Constants
-    public final static String DATA_PATH = "./src/main/resources/static/data";
     public final static String DATA_MAPPING = "/data";
 
     private final PictureRepository pictureRepo;
@@ -27,6 +29,8 @@ public class PictureController {
     @GetMapping("/all")
     @ResponseBody
     public List<String> all(){
+        LOGGER.info("Request: GET /all");
+
         List<Picture> pictures = pictureRepo.findAll();
         ArrayList<String> urls = new ArrayList<>();
 
