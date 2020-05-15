2ffff93f841caed93bb09f90c2507948717d8bc7
Paul Zhu <zhz433@ucsd.edu>
2020-05-15 05:00:30 -0700

    Try encoding filter

--- server/src/main/java/com/memegle/server/ServerApplication.java
+++ server/src/main/java/com/memegle/server/ServerApplication.java
@@ -2,6 +2,8 @@ package com.memegle.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -11,4 +13,12 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	// Set default encoding to utf-8
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
