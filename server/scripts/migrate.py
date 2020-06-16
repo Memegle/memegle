b24dcc4fc6ee5be3d5166c0aca13112803e3391e
Paul Zhu <zhz433@ucsd.edu>
2020-06-15 19:02:37 -0700

    modified scripts

--- server/scripts/migrate.py
+++ server/scripts/migrate.py
@@ -2,11 +2,15 @@
 from pymongo import MongoClient
 from pprint import pprint
 from os import listdir, rename, remove
-from os.path import isfile, join
+from os.path import isfile, join, exists, splitext
 import datetime
+from shutil import copyfile
 
-RAW_DATA_PATH = '../raw/'
-STATIC_DATA_PATH = '../src/main/resources/static/data/'
+# COPY is used for debugging this script, normally you don't need to copy, which cost you more disk space.
+COPY = True
+
+RAW_DATA_PATH = './raw/'
+STATIC_DATA_PATH = './src/main/resources/static/data/'
 URL_PREFIX = 'http://memegle.qicp.vip/data/'
 
 client = MongoClient(port=27017)
@@ -34,19 +38,13 @@ pic_col = db.pictures
 img_files = [f for f in listdir(RAW_DATA_PATH) if isfile(join(RAW_DATA_PATH, f))]
 insert_lst = []
 already_exist = []
-name_too_long = []
-
-def byte_length(s):
-    return len(s.encode('utf-8'))
 
 for img in img_files:
-    if byte_length(img) >= 255:
-        name_too_long.append(img)
-        continue
-
     if pic_col.find_one({'name': img}) is not None:
         already_exist.append(img)
         continue
+
+    _, ext = splitext(img)
 
     seq += 1
 
@@ -54,25 +52,32 @@ for img in img_files:
         '_id': seq,
         'name': img,
         'dateUpdated': datetime.datetime.utcnow(),
-        'url': URL_PREFIX + img,
+        'url': URL_PREFIX + str(seq) + ext,
         '_class': 'com.memegle.server.Picture.Picture'
     }
     insert_lst.append(d)
 
-    # move file to static
-    try:
-        rename(join(RAW_DATA_PATH, img), join(STATIC_DATA_PATH, img))
-    except FileExistsError:
-        remove(join(RAW_DATA_PATH, img))
 
+if len(insert_lst) > 0:
+    pic_col.insert_many(insert_lst)
+    seq_col.update_one({'_id': 'picture_sequence'}, {'$set': {'seq': seq}})
 
-seq_col.update_one({'_id': 'picture_sequence'}, {'$set': {'seq': seq}})
-pic_col.insert_many(insert_lst)
+    for i in insert_lst:
+        img = i['name']
+        id = i['_id']
 
-if len(name_too_long) > 0:
-    print('The following imgs have a too long filename:')
-for i in name_too_long:
-    print(i)
+        _, ext = splitext(img)
+        # move file to static
+        source = join(RAW_DATA_PATH, img)
+        dest = join(STATIC_DATA_PATH, str(id) + ext)
+        if exists(dest):
+            remove(dest)
+
+        if COPY:
+            copyfile(source, dest)
+        else:
+            rename(source, dest)
+
 if len(already_exist) > 0:
     print('The following imgs already exists in the db:')
 for i in already_exist:
