5867ffb516528cf1aa7e78701072deb127adc153
tonyh4156 <tonyhu4156@gmail.com>
2020-06-18 15:48:34 -0700

    welcome page in progress

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -6,27 +6,18 @@ class Main extends Component {
     this.state = {}
   }
 
-  getRandMeme() {
-    const Http = new XMLHttpRequest();
-    const url='http://localhost:8080/random';
-    Http.open("GET", url);
-    Http.send();
-
-    /* Http.onreadystatechange = (e) => {
-        console.log(Http.responseText);
-        document.getElementById('meme').src = Http.responseText
-    } */
-  }
-
   render() {
     return(
-      <div>
-        <h1>欢迎来到Memegle！</h1>
-
-        <p>Memegle仍在开发中，来点表情包？</p>
-
-        <button type="button" onClick={this.getRandMeme}>来一包！</button>
-      </div>
+        <div className="App">
+          <button className="Button-about">About</button>
+          <button className="Button-contactUs">Contact Us</button>
+          <div className="App-header">
+            <img src={require('./Memegle.png')} className="App-logo" />
+            <input type="text" placeholder="Search..."></input>
+          </div>
+          <button className="Button-search"><b>Search</b></button>
+          <button className="Button-feelingLucky">Feeling Lucky?</button>
+        </div>
     )
   }
 }
