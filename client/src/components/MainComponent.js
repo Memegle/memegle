4600fc7977c2a5127e16b9678f815cbf268a1b74
tonyh4156 <tonyhu4156@gmail.com>
2020-06-18 16:51:43 -0700

    search button working

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../css/Welcome.css';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-  render() {
-    return(
-        <div className="App">
-          <button className="Button-about">About</button>
-          <button className="Button-contactUs">Contact Us</button>
-          <div className="App-header">
-            <img src={require('./Memegle.png')} className="App-logo" />
-            <input type="text" placeholder="Search..."></input>
-          </div>
-          <button className="Button-search"><b>Search</b></button>
-          <button className="Button-feelingLucky">Feeling Lucky?</button>
-        </div>
-    )
-  }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.keyPressed = this.keyPressed.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+        console.log(this.state.value);
+
+    }
+
+    keyPressed(event) {
+        if (event.key === 'Enter') {
+            this.handleSubmit(event);
+        }
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <button className='Button-about'>About</button>
+                <button className='Button-contactUs'>Contact Us</button>
+                <div className='App-header'>
+                    <img src={require('../assets/Memegle.png')} className='App-logo' />
+                    <input type='text' value={this.state.value} onKeyPress={this.keyPressed} onChange={this.handleChange}></input>
+                </div>
+                <button className='Button-search' onClick={this.handleSubmit}><b>Search</b></button>
+                <button className='Button-feelingLucky'>Feeling Lucky?</button>
+            </div>
+        );
+    }
 }
 
-export default Main;
+export default Welcome;
