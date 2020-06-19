f4924fee295f5fbd3963957928243635b917f02b
Paul Zhu <zhz433@ucsd.edu>
2020-06-18 20:41:59 -0700

    update env var

--- server/src/main/java/com/memegle/server/ServerApplication.java
+++ server/src/main/java/com/memegle/server/ServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
