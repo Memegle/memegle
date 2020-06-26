dbfbb4a8c8e59b267130dd89263b8066d5ccf4ff
Paul Zhu <zhz433@ucsd.edu>
2020-06-26 16:41:34 -0700

    changed dockerfile

--- server/src/test/java/com/memegle/server/ServerApplicationTests.java
+++ server/src/test/java/com/memegle/server/ServerApplicationTests.java
@@ -3,11 +3,16 @@ package com.memegle.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// read this tutorial https://www.baeldung.com/spring-boot-testing
 @SpringBootTest
 class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Paul: currently the test cannot be passed successfully due to an elasticsearch warning.
+		// I'm following these two threads and hoping to get a solution.
+		// https://github.com/elastic/elasticsearch/issues/54501
+
 	}
 
 }
