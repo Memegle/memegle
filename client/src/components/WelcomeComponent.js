27166a957ddcebf4a56bb6a72c42f0bb58f4c8b6
liyanxun <liyanxun1128@gmail.com>
2020-07-03 19:24:30 -0700

    finish welcome ui

--- client/src/components/WelcomeComponent.js
+++ client/src/components/WelcomeComponent.js
@@ -41,18 +41,23 @@ class Welcome extends React.Component {
         }
         else {
             return (
-                <div>
-                    <div className='top-buttons-div'>
-                        <button className='top-buttons'>About</button>
-                        <button className='top-buttons'>Contact Us</button>
+                <div className='container'>
+                    <div className='row home-header'>
+                        <div className='row home-logo-div'>
+                            <img src={require('../assets/Memegle.png')} className='home-logo' alt='none'/>
+                        </div>
+                        <div className='row home-search-bar-div'>
+                            <div className='col-md-9 center'>
+                                <input className='home-search-bar' type='text' placeholder='热门关键词' 
+                                    value={this.state.value} onKeyPress={this.keyPressed} onChange={this.handleChange}></input>
+                            </div>
+                            <div className='col-md-3 center'>
+                                <button className='button-search' onClick={this.handleSubmit}><b>搜图 :)</b></button>
+                            </div>
+                        </div>
                     </div>
-                    <div className='home-header'>
-                        <img src={require('../assets/Memegle.png')} className='home-logo' alt='none'/>
-                        <input className='home-search-bar' type='text' value={this.state.value} onKeyPress={this.keyPressed} onChange={this.handleChange}></input>
-                    </div>
-                    <div className='bottom-buttons-div'>
-                        <button className='Button-search' onClick={this.handleSubmit}><b>Memegle Search</b></button>
-                        <button className='Button-feelingLucky'>Feeling Lucky?</button>
+                    <div className='row text center'>
+                        网络表情符号最早来自于1982年美国卡内基梅隆大学Scott E·Fahlman教授在BBS上首次使用的ASCII码”:-)”表示微笑。
                     </div>
                 </div>
             );
