b85efe3a1d054f2d411e7b1818eed78ce3b4db82
Paul Zhu <zhz433@ucsd.edu>
2020-07-10 20:13:21 -0700

    nice

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose stop server
+docker-copmose stop client
+pip3 install -r ./server/scripts/requirement.txt
+docker-compose build
+docker-compose run --rm build_server gradle build -i --no-daemon -x test
+docker-compose run --rm clinet cp -r /home/gradle/cache_home /home/gradle/.gradle
+docker-compose run --rm client npm install
--- server/scripts/init-boot.sh
+++ server/scripts/init-boot.sh
@@ -2,6 +2,7 @@
 
 # https://linuxparrot.com/elasticsearch-healthcheck/
 es_status=-1
+PING_COUNT=0
 uri="$ES_URI/_cluster/health"
 
 # Not checking for mongo status bc it's already done in mongo_init.sh
@@ -10,6 +11,13 @@ echo "Waiting for ES cluster to be on..."
 until [ $es_status -eq 200 ]; do
   sleep 2
   es_status=$(curl --write-out %{http_code} --silent --output /dev/null $uri)
+  PING_COUNT=$((PING_COUNT+1))
+  # Stop after 1 min
+  if [ $PING_COUNT -gt 30 ]
+  then
+    echo Connection failed, stopping...
+    exit 1
+  fi
 done
 
 echo "ES cluster is now on!"
