57af30d22f79ef2446d1f9c06525b1eb717ce8aa
Paul Zhu <zhz433@ucsd.edu>
2020-07-12 15:27:32 -0700

    remove a flow line

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,4 @@ docker-compose stop server
 pip3 install -r ./server/scripts/requirement.txt
 docker-compose build
 docker-compose run --rm build_server gradle build -i --no-daemon -x test
-docker-compose run --rm clinet cp -r /home/gradle/cache_home /home/gradle/.gradle
 docker-compose run --rm client npm install
