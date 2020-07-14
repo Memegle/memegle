9b85c73a62acb4fa7de3873b98722e9850cd72f1
Bob Zhang <chz026@ucsd.edu>
2020-07-14 14:22:14 -0700

    added pillow requirement for migrate.py

--- server/scripts/migrate.py
+++ server/scripts/migrate.py
@@ -5,6 +5,7 @@ from os import listdir, rename, remove
 from os.path import isfile, join, exists, splitext
 import datetime
 from shutil import copyfile
+from PIL import Image
 
 # COPY is used for debugging this script, normally you don't need to copy, which cost you more disk space.
 COPY = True
@@ -48,12 +49,18 @@ for img in img_files:
 
     seq += 1
 
+    picture = Image.open(img)
+    width, height = picture.size
+
     d = {
         '_id': seq,
         'name': img,
         'dateUpdated': datetime.datetime.utcnow(),
         'urlSuffix': URL_PREFIX + str(seq) + ext,
-        '_class': 'com.memegle.server.Picture.Picture'
+        '_class': 'com.memegle.server.Picture.Picture',
+        'width': width,
+        'height': height
+        # add height & width
     }
     insert_lst.append(d)
 
