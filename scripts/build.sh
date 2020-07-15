e1d49ae1359662cafff17ad752b1cc0e2a41b9d4
Paul Zhu <zhz433@ucsd.edu>
2020-07-15 00:20:29 -0700

    npm to yarn

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ docker-compose stop server
 pip3 install -r ./server/scripts/requirements.txt
 docker-compose build
 docker-compose run --rm build_server gradle build -i --no-daemon -x test
-docker-compose run --rm client npm install
+# use yarn to avoid the annoying npm audit
+docker-compose run --rm client yarn
