80456191e1fbf8aa2605cde810bc8d0e2187aca5
Paul Zhu <zhz433@ucsd.edu>
2020-07-16 04:52:04 -0700

    a working version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker-compose stop server
 pip3 install -r ./server/scripts/requirements.txt
 docker-compose build
-docker-compose run --rm build_server gradle build -i --no-daemon -x test
+docker-compose run --rm server gradle build -i -x test --project-cache-dir=//home/gradle/cache
 # use yarn to avoid the annoying npm audit
 docker-compose run --rm client yarn
