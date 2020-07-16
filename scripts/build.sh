ac0c0370d1fdfd4523d4a46c7ee40be223c5f658
Paul Zhu <zhz433@ucsd.edu>
2020-07-16 05:55:16 -0700

    working live reload for spring boot

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 pip3 install -r ./server/scripts/requirements.txt
 docker-compose build
-docker-compose run --rm server gradle build -i -x test --project-cache-dir=//home/gradle/cache
+docker-compose run --rm server gradle build -i -x test
 # use yarn to avoid the annoying npm audit
 docker-compose run --rm client yarn
