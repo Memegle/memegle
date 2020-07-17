8a613b894099716594da754b82fa19725dbb5bef
Paul Zhu <35409183+vinovo@users.noreply.github.com>
2020-07-16 18:44:15 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 pip3 install -r ./server/scripts/requirements.txt
-dos2unix ./scripts/*
-dos2unix ./server/scripts/*
 docker-compose build
 docker-compose run --rm server gradle build -i -x test
 # use yarn to avoid the annoying npm audit
