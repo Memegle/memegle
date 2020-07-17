bbb5cb51787a7003aa40f705b288098582183f6c
Paul Zhu <35409183+vinovo@users.noreply.github.com>
2020-07-16 18:43:09 -0700

    dos2unix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pip3 install -r ./server/scripts/requirements.txt
+dos2unix ./scripts/*
+dos2unix ./server/scripts/*
 docker-compose build
 docker-compose run --rm server gradle build -i -x test
 # use yarn to avoid the annoying npm audit
