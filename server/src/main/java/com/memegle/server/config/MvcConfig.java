e8750a34c40d564e7423d595993dda5cb445d079
Paul Zhu <zhz433@ucsd.edu>
2020-07-19 18:22:00 -0700

    add to existing resource handler

--- server/src/main/java/com/memegle/server/config/MvcConfig.java
+++ server/src/main/java/com/memegle/server/config/MvcConfig.java
@@ -4,17 +4,17 @@
 
 package com.memegle.server.config;
 
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**").addResourceLocations("file:/root/memegle/images/");
-        registry.addResourceHandler().addResourceLocations()
     }
 }
