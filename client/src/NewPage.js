69755b93b5c04f47b216af580ea771c68e64f582
ZongchenLi <zoli@ucsd.edu>
2020-12-27 04:11:29 -0800

    Send get-request to backend in order to get the total number of images stored in server. Print out that number on the page after searching "memegle".

--- client/src/NewPage.js
+++ client/src/NewPage.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
 
+export const func = async () => {
+    const url = 'http://www.memegle.live:8080/count';
+    let result = '';
+    try {
+        const response = await fetch(url);
+        result = await response.text();
+    } catch (e) {
+        throw e;
+    }
+
+    return result;
+}
+let num='';
+
+func().then(result => {
+    num = result;
+})
+
+
 const NewPage = () => {
 return(
-    <p>The total number of images is:  </p>
+    <p>The total number of images is: {num} </p>
 )
 };
 
