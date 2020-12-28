4857d2577ce788b70b34b85a9536214c01203d14
Jessica <zhz386@ucsd.edu>
2020-12-27 16:44:30 -0800

    changed paddlepaddle verions to 2.0.0rc1 and updated requirements

--- server/scripts/paddle-setup.sh
+++ server/scripts/paddle-setup.sh
@@ -3,7 +3,7 @@
 count=0
 
 python3 -m pip install --upgrade pip || ((count++))
-python3 -m pip install paddlepaddle==2.0.0b0 -i https://mirror.baidu.com/pypi/simple || ((count++))
+python3 -m pip install paddlepaddle==2.0.0rc1 -i https://mirror.baidu.com/pypi/simple || ((count++))
 python3 -m pip install -r ./scripts/paddle-req.txt || ((count++))
 python3 -m pip install paddleocr || ((count++))
 
@@ -12,3 +12,4 @@ if ((count != 0)); then
 else
   printf "\n\nPaddleOCR has been installed successfully\n"
 fi
+
