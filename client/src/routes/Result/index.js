60ec425666509b257063aea30e5dd52a8ee25ece
Paul Zhu <35409183+vinovo@users.noreply.github.com>
2021-01-11 15:09:36 -0800

    Revert "Frank li/onboarding task (#99)" (#105)
    This reverts commit af556457

--- client/src/routes/Result/index.js
+++ client/src/routes/Result/index.js
@@ -196,9 +196,6 @@ class Result extends Component {
     }
 
     render() {
-        if(this.keyword ==='memegle'){
-            return <Redirect to='newpage'/>;
-        }
         if (this.state.toWelcome) {
             return <Redirect to='welcome'/>;
         } else if (this.state.newSearch) {
