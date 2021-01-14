0aa292c7362718a67de490b44b0b267d115cf3dc
ZongchenLi <zoli@ucsd.edu>
2021-01-14 00:59:21 -0800

    move the function posting request to backend under /actions.

---
+++ client/src/actions/count.js
@@ -0,0 +1,12 @@
+
+export const count = async () => {
+    const url = 'http://www.memegle.live:8080/count';
+    let result = '';
+    try {
+        const response = await fetch(url);
+        result = await response.text();
+    } catch (e) {
+        throw e;
+    }
+    return result;
+}
