de14d3d7376a9d4df96d046fe4a3aa1fa837a64f
Gin594 <34392120+Gin594@users.noreply.github.com>
2021-01-28 10:22:18 -0800

    Chris/email registration (#107)
    * spring security authentication added
    
    * added user bean detail
    
    * added duplicate user registration check
    
    * realse permission when access home page without authentication
    
    * Revert "added duplicate user registration check"
    
    This reverts commit 2f12e013d7ce8779a09fbb1ee5f490a2d0e74a0e.
    
    d
    
    * Chris/send-email
    
    * Verify password must be at least 8 chars, no space and at least one letter.
    
    * fix one typo
    
    * Chris/activation-finished
    
    * Chris/activation-finished
    
    Co-authored-by: ZongchenLi <zoli@ucsd.edu>

---
+++ server/src/main/java/com/memegle/server/service/CustomerDetails.java
@@ -0,0 +1,56 @@
+package com.memegle.server.service;
+
+import com.memegle.server.model.CustomUserDetails;
+import com.memegle.server.model.User;
+import com.memegle.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDetails implements UserDetailsService{
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User findByUserName(String username) {
+        return userRepository.findByUserName(username);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User findByUserNameAndPassword(String username, String password) {
+        User user = findByUserName(username);
+        if (user != null) {
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUserName(username);
+        return CustomUserDetails.toCustomUserDetails(user);
+    }
+}
