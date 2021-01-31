0e14318d99883b25394fe5acc1a37a9056c968a7
ZongchenLi <zoli@ucsd.edu>
2021-01-31 14:28:40 -0800

    update models to fit DB schema

--- server/src/main/java/com/memegle/server/model/PictureSearch.java
+++ server/src/main/java/com/memegle/server/model/PictureSearch.java
@@ -1,4 +1,5 @@
 package com.memegle.server.model;
+import org.bson.types.ObjectId;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.memegle.server.util.Constants;
@@ -9,17 +10,21 @@ import org.springframework.data.elasticsearch.annotations.Score;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 /**
  * We need this new entity bc we can't have 2 repo for Picture class at the same time.
  */
 @Document(indexName = "memegle.pictures", type = "_doc")
 public class PictureSearch {
+
     @Id
-    private String id;   // id is stored as a string in elasticsearch
+    private ObjectId id;   // id is stored as a string in elasticsearch           
+    private String title;
+    private String source;
+    private String mediaUrl;
 
     // Whenever the schema of Picture changes, copy fields and getter methods from Picture class
     /* paste start */
-    private String name;
     private String filetype;
     private Date dateUpdated;
     private int width;
@@ -36,16 +41,19 @@ public class PictureSearch {
     @Score
     private float searchScore;    // Read-only value, auto-populated by elastic repo
 
-    private String urlSuffix;
-
     public PictureSearch() {}
 
-    public String getId() {return id;}
+   
+   //Update to fit DB schema
+    public String getId() {return this.id;}
+    public String getTitle() {return this.title;}                //------------------------&&&&&
+    public String getFiletype() {return this.filetype;}
+    public String getSourceUrl(){return this.sourceUrl}
+    public String getMediaUrl() {return this.mediaUrl;}
+
 
     /* paste start */
-    public String getName() {return this.name;}
-    public String getFiletype() {return this.filetype;}
-    public String getUrlSuffix() {return this.urlSuffix;}
+    
     public Date getDateUpdated() {return this.dateUpdated;}
     public int getWidth() {return this.width;}
     public int getHeight() {return this.height;}
@@ -59,8 +67,4 @@ public class PictureSearch {
 
     public float getSearchScore() {return this.searchScore;}
 
-    @JsonProperty("fullUrl")
-    public String getFullUrl() {
-        return Constants.BASE_URL + Constants.IMAGE_MAPPING + this.urlSuffix;
-    }
 }
