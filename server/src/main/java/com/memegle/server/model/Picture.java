330b9eba645d9c30dada7a5d798ae2adc0f67ff6
ZongchenLi <zoli@ucsd.edu>
2021-02-06 18:39:19 -0800

    Update fields and getters, setters of pict and pcitSearch.java

--- server/src/main/java/com/memegle/server/model/Picture.java
+++ server/src/main/java/com/memegle/server/model/Picture.java
@@ -15,24 +15,30 @@ public class Picture {
     @Transient
     public static final String SEQUENCE_NAME = "picture_sequence";
 
+
+    //field only in Picture.java
+  
+
+    //field shared with PictureSearch.java  (total:12)
     @Id
-    private ObjectId id;
+    private ObjectId id;                 //########### Need to be tested 
     private String sourceUrl;
     private String title;   
     private String source;
     private String mediaUrl;
     private String ext;
-
-    private String filetype;
-    private Date dateUpdated;
     private int width;
     private int height;
     private ArrayList<String> texts;
     private ArrayList<Float> confidences;
     private ArrayList<ArrayList<ArrayList<Integer>>> boundingBoxes;
-    private ArrayList<String> tags;
-    private long like;
-    private long dislike;
+    private Date dateCreated;
+
+
+
+
+
+
 
     @Transient
     private float searchScore;
@@ -43,48 +49,48 @@ public class Picture {
     public static Picture fromPictureSearch(PictureSearch search) {
         Picture picture = new Picture();
         // set all values
-        picture.setId(search.getId());                      //------------------------
-        picture.setTitle(search.getTitle());                //------------------------
-        picture.setFiletype(search.getFiletype());
+        picture.setId(search.getId());                     
+        picture.setTitle(search.getTitle());                
+        picture.setSource(search.getSource());
+        picture.setSourceUrl(search.getSourceUrl());
+        picture.setMediaUrl(search.getMediaUrl());
+        picture.setExt(search.getExt());
+        picture.setDateCreated(search.getDateCreated());
+        picture.setWidth(search.getWidth());
+        picture.setHeight(search.getHeight());
+        picture.setTexts(search.getTexts());
+        picture.setConfidences(search.getConfidences());
+        picture.setBoundingBoxes(search.getBoundingBoxes());
+
         return null;
     }
  
-    //adjust getters for variables id an title
-    public ObjectId getId() {return this.id;}
-    public String getTitle() {return this.title;}
-    public String getSourceUrl(){return this.sourceUrl}
+    //Getters
+
+    public ObjectId getId() {return this.id;}        //########### Need to be tested 
+    public String getTitle() {return this.title; }
+    public String getSource() {return this.source; }
+    public String getSourceUrl(){return this.sourceUrl; }
     public String getMediaUrl() {return this.mediaUrl;}
     public String getExt() {return this.ext;}
-
-    public String getFiletype() {return this.filetype;}
-    public Date getDateUpdated() {return this.dateUpdated;}
+    public Date getDateCreated() {return this.dateCreated;}
     public int getWidth() {return this.width;}
     public int getHeight() {return this.height;}
     public ArrayList<String> getTexts() {return texts;}
     public ArrayList<Float> getConfidences() {return confidences;}
     public ArrayList<ArrayList<ArrayList<Integer>>> getBoundingBoxes() {return boundingBoxes;}
-    public ArrayList<String> getTags() {return tags;}
-    public long getLike() {return like;}
-    public long getDislike() {return dislike;}
 
-    public float getSearchScore() {return searchScore;}
-
-
-    public void setId(ObjectId id) {this.id = id;}                 
-    public void setTitle(String title) {this.title = title;}        
+    //Setters 
+    public void setId(ObjectId id) {this.id = id;}      //########### Need to be tested 
+    public void setTitle(String title) {this.title = title;}     
+    public void setSource(String src) {this.source = src;}      
     public String setSourceUrl(String sourceUrl) {this.sourceUrl = sourceUrl;}
     public void setMediaUrl(String mediaUrl) {this.mediaUrl = mediaUrl;}
     public void setExt(String ext) {this.ext = ext;}
-
-
-    public void setFiletype(String filetype) {this.filetype = filetype;}
-    public void setDateUpdated(Date date) {this.dateUpdated = date;}
+    public void setDateCreated(Date date) {this.dateCreated = date;}
     public void setWidth(int w) {this.width = w;}
     public void setHeight(int h) {this.height = h;}
     public void setTexts(ArrayList<String> text) {this.texts = text;}
     public void setConfidences(ArrayList<Float> confidence) {this.confidences = confidence;}
     public void setBoundingBoxes(ArrayList<ArrayList<ArrayList<Integer>>> boundingBoxes) {this.boundingBoxes = boundingBoxes;}
-    public void setTags(ArrayList<String> tags) {this.tags = tags;}
-    public void setLike(long like) {this.like = like;}
-    public void setDislike(long dislike) {this.dislike = dislike;}
 }
--- server/src/main/java/com/memegle/server/model/PictureSearch.java
+++ server/src/main/java/com/memegle/server/model/PictureSearch.java
@@ -17,60 +17,47 @@ import java.util.Date;
 @Document(indexName = "memegle.pictures", type = "_doc")
 public class PictureSearch {
 
-    @Id
-    // Whenever the schema of Picture changes, copy fields and getter methods from Picture class
-
-
+    //field only in PictureSearch.java
+      private int score;
+    
+    //field shared with Picture.java
     /* paste start */
-    private ObjectId id;   
-    private String title;
+    @Id
+    private String id;
+    private String sourceUrl;
+    private String title;   
     private String source;
     private String mediaUrl;
     private String ext;
-    
     private String filetype;
-    private Date dateUpdated;
     private int width;
     private int height;
     private ArrayList<String> texts;
     private ArrayList<Float> confidences;
     private ArrayList<ArrayList<ArrayList<Integer>>> boundingBoxes;
-    private ArrayList<String> tags;
-    private long like;
-    private long dislike;
     /* paste end */
 
 
-    @Score
-    private float searchScore;    // Read-only value, auto-populated by elastic repo
-
-    public PictureSearch() {}
-
    
+    public PictureSearch() {}
    //Update to fit DB schema
-  
-    /* paste start */
-    public String getId() {return this.id;}
-    public String getTitle() {return this.title;}         
-    public String getFiletype() {return this.filetype;}
-    public String getSourceUrl(){return this.sourceUrl}
+
+    //Getters
+    public String getScore()) {return this.score; }
+
+   /* paste start */ 
+    public String getId() {return this.id;}        
+    public String getTitle() {return this.title; }
+    public String getSource() {return this.source; }
+    public String getSourceUrl(){return this.sourceUrl; }
     public String getMediaUrl() {return this.mediaUrl;}
     public String getExt() {return this.ext;}
-
-    public Date getDateUpdated() {return this.dateUpdated;}
+    public Date getDateCreated() {return this.dateCreated;}
     public int getWidth() {return this.width;}
     public int getHeight() {return this.height;}
     public ArrayList<String> getTexts() {return texts;}
     public ArrayList<Float> getConfidences() {return confidences;}
     public ArrayList<ArrayList<ArrayList<Integer>>> getBoundingBoxes() {return boundingBoxes;}
-    public ArrayList<String> getTags() {return tags;}
-    public long getLike() {return like;}
-    public long getDislike() {return dislike;}
-
-
-
     /* paste end */
-
-    public float getSearchScore() {return this.searchScore;}
 
 }
